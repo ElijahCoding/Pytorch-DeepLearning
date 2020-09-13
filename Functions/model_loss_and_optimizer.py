55a94f189cb6d9cc761c41b360e783bdc87bd835
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-13 19:18:23 +0300

    optimizer

---
+++ Functions/model_loss_and_optimizer.py
@@ -0,0 +1,67 @@
+# 1) Design model (input, output, forward pass with different layers)
+# 2) Construct loss and optimizer
+# 3) Training loop
+#       - Forward = compute prediction and loss
+#       - Backward = compute gradients
+#       - Update weights
+
+import torch
+import torch.nn as nn
+
+# Linear regression
+# f = w * x
+
+# here : f = 2 * x
+
+# 0) Training samples, watch the shape!
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+
+n_samples, n_features =  X.shape
+print(f'#samples: {n_samples}, #features: {n_features}')
+
+X_test = torch.tensor([5], dtype=torch.float32)
+
+intput_size = n_features
+output_size = n_features
+
+model = nn.Linear(intput_size, output_size)
+
+'''
+class LinearRegression(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegression, self).__init__()
+        # define diferent layers
+        self.lin = nn.Linear(input_dim, output_dim)
+    def forward(self, x):
+        return self.lin(x)
+model = LinearRegression(input_size, output_size)
+'''
+
+print(f'Prediction before training: f(5) = {model(X_test).item():.3f}')
+
+learning_rate = 0.01
+n_iters = 100
+
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+for epoch in range(n_iters):
+    y_predicted = model(X)
+
+    # loss
+    l = loss(Y, y_predicted)
+
+    # calculate gradients = backward pass
+    l.backward()
+
+    # update weights
+    optimizer.step()
+
+    optimizer.zero_grad()
+
+    if epoch % 10 == 0:
+        [w, b] = model.parameters()  # unpack parameters
+        print('epoch ', epoch + 1, ': w = ', w[0][0].item(), ' loss = ', l)
+
+print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
