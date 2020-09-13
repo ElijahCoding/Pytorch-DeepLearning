b437182a9d0ad9551478ec66f6aa8044bce4474a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-13 22:27:43 +0300

    building structure

---
+++ Functions/dataloader.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+class WineDataset(Dataset):
+    def __init__(self):
+        # data loading
+    pass
+
+    def __getitem__(self, index):
+        # dataset[0]
+        pass
+
+    def __len__(self):
+        # len(dataset)
+        pass
