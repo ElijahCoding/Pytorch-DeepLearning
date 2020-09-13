06e6f948b2c6f1bcb8000a05c6953e1a9e4269cf
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-13 22:55:23 +0300

    dataset built

--- Functions/dataloader.py
+++ Functions/dataloader.py
@@ -7,12 +7,15 @@ import math
 class WineDataset(Dataset):
     def __init__(self):
         # data loading
-    pass
+        xy = np.loadtxt('./datasets/wine.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.x = torch.from_numpy(xy[:, 1:])
+        self.y = torch.from_numpy(xy[:, [0]]) # n_samples, 1
+        self.n_samples = xy.shape[0]
 
     def __getitem__(self, index):
         # dataset[0]
-        pass
+        return self.x[index], self.y[index]
 
     def __len__(self):
         # len(dataset)
-        pass
+        return self.n_samples
