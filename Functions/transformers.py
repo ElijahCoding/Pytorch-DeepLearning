4bb3bd2b3f92c686561a945535c821fceda16931
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-14 21:48:31 +0300

    dataset transformed

--- Functions/transformers.py
+++ Functions/transformers.py
@@ -34,7 +34,7 @@ import numpy as np
 
 class WineDataset(Dataset):
     def __init__(self, transform=None):
-        xy = np.load('./datasets/wine.csv', delimeter=',', dtype=np.float32, skiprows=1)
+        xy = np.loadtxt('./datasets/wine.csv', delimiter=',', dtype=np.float32, skiprows=1)
         self.n_samples = xy.shape[0]
 
         self.x = xy[:, 1:]
@@ -59,7 +59,22 @@ class ToTensor:
         inputs, targets = sample
         return torch.from_numpy(inputs), torch.from_numpy(targets)
 
+class MulTransform:
+    def __init__(self, factor):
+        self.factor = factor
 
+    def __call__(self, sample):
+        inputs, target = sample
+        inputs *= self.factor
+        return inputs,  target
 
-dataset = WineDataset()
+dataset = WineDataset(transform=ToTensor())
+first_data = dataset[0]
+features, labels = first_data
+print(type(features), type(labels))
 
+composed = torchvision.transforms.Compose([ToTensor(), MulTransform(2)])
+dataset = WineDataset(transform=composed)
+first_data = dataset[0]
+features, labels = first_data
+print(type(features), type(labels))
