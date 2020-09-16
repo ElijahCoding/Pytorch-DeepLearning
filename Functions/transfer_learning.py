a3316fa4bec4c7fc1479f3121d6532a99d794157
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-16 22:44:04 +0300

    transfering learning , importing data

---
+++ Functions/transfer_learning.py
@@ -0,0 +1,36 @@
+# ImageFolder
+# Scheduler
+# Transfer Learning
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
+
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+}
